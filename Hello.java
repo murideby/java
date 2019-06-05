284d7d1f34f1fddeebdf9acb70e66979860a6688
murideby <44686075+murideby@users.noreply.github.com>
2019-06-05 19:50:27 -0300

    Adicionei o arquivo hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+*Hello World Java
+* @Débora Murielle
+*/
+
+
+public class Hello{
+	// método principal
+	public static void main (String[] args){
+		System.out.println("Hello World");
+	}
+}
